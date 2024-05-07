dff4d09eb30504f4cedd6a64a96f37037dc03c0b
Escbrk <69437294+Escbrk@users.noreply.github.com>
2024-05-07 00:19:51 -0600

    upd

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,12 +1,12 @@
 type User = {
-  name?: string;
-  surname?: string;
-  email?: string;
-  password?: string;
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
 };
 
 function createOrUpdateUser(initialValues: Partial<User>): User {
-  return { ...initialValues };
+  return { ...initialValues } as User;
 }
 
 createOrUpdateUser({ email: "user@mail.com", password: "password123" });
